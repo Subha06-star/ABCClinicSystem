1f2a0b2cd2c73ea3bf62155b6d6e16271f7812e9
s76804746 <s76804746@gmail.com>
2026-03-29 23:19:13 -0400

    Added User model

--- obj/Debug/net10.0/ABCClinicSystem.AssemblyInfo.cs
+++ obj/Debug/net10.0/ABCClinicSystem.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ABCClinicSystem")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0de443b9ff11dd95523693dd66af77d6f2ab77fa")]
 [assembly: System.Reflection.AssemblyProductAttribute("ABCClinicSystem")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ABCClinicSystem")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
